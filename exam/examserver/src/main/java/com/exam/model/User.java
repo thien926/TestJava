e0d8b017f79a7551d6a27de18ba35a41b5906edf
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2023-03-27 22:54:36 +0700

    create DB user and role

---
+++ exam/examserver/src/main/java/com/exam/model/User.java
@@ -0,0 +1,31 @@
+package com.exam.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private boolean enabled = true;
+	private String profile;
+}
