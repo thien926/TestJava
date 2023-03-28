413b33997ca044ea0c6218512d1e362d20ca3e76
Thien Nguyen <thien926@users.noreply.github.com>
2023-03-29 06:45:53 +0700

    add UserRole

--- exam/examserver/src/main/java/com/exam/model/Role.java
+++ exam/examserver/src/main/java/com/exam/model/Role.java
@@ -1,9 +1,17 @@
 package com.exam.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,4 +29,8 @@ public class Role {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String roleName;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "role")
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
 }
--- exam/examserver/src/main/java/com/exam/model/User.java
+++ exam/examserver/src/main/java/com/exam/model/User.java
@@ -1,9 +1,17 @@
 package com.exam.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,4 +36,8 @@ public class User {
 	private String phone;
 	private boolean enabled = true;
 	private String profile;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
 }
