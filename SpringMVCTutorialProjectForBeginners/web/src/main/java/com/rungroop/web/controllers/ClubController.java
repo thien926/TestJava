db0d2e535b3aa20d062a6707c8c25dfc62c996be
Thien Nguyen <tructruong.070202@gmail.com>
2024-08-22 21:47:46 +0700

    CRUD club

--- SpringMVCTutorialProjectForBeginners/web/src/main/java/com/rungroop/web/controllers/ClubController.java
+++ SpringMVCTutorialProjectForBeginners/web/src/main/java/com/rungroop/web/controllers/ClubController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rungroop.web.dtos.ClubDto;
+import com.rungroop.web.entities.Club;
+import com.rungroop.web.mappers.ClubMapper;
 import com.rungroop.web.services.ClubService;
 
 @Controller
@@ -19,7 +23,23 @@ public class ClubController {
 	public ModelAndView listClubs(ModelAndView mav) {
 		List<ClubDto> clubs = clubService.findAllClubs();
 		mav.addObject("clubs", clubs);
-		mav.setViewName("clubs/club-list");
+		mav.setViewName("clubs/clubs-list");
+		return mav;
+	}
+	
+	@GetMapping("/clubs/new")
+	public ModelAndView createClubForm(ModelAndView mav) {
+		Club club = new Club();
+		mav.addObject("club", ClubMapper.INSTANCE.toDto(club));
+		mav.setViewName("clubs/clubs-create");
+		return mav;
+	}
+	
+	@PostMapping("/clubs/new")
+	public ModelAndView saveClub(@ModelAttribute("club") ClubDto clubDto
+			, ModelAndView mav) {
+		clubService.saveClub(clubDto);
+		mav.setViewName("redirect:/clubs");
 		return mav;
 	}
 }
